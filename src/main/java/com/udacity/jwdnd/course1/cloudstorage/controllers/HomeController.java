8cf67922ec0dc2573d23edc8e656f6b24ff80af4
Eduardo Alvarez <ealvarez0896@gmail.com>
2020-04-04 01:48:34 -0600

    Messages Working

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/HomeController.java
@@ -1,14 +1,22 @@
 package com.udacity.jwdnd.course1.cloudstorage.controllers;
 
 
+import com.udacity.jwdnd.course1.cloudstorage.model.Credentials;
+import com.udacity.jwdnd.course1.cloudstorage.model.Files;
 import com.udacity.jwdnd.course1.cloudstorage.model.Notes;
 import com.udacity.jwdnd.course1.cloudstorage.model.NotesMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
 @ContextConfiguration(locations = "classpath:/beans.xml")
@@ -17,9 +25,38 @@ public class HomeController {
     @Autowired
     private NotesMapper notesMapper;
 
+    @PostMapping("/home/notes/add")
+    public RedirectView greetingSubmit(@ModelAttribute Notes note, RedirectAttributes attributes) {
+
+        System.out.println("holaaa");
+        System.out.println(note.getNotetitle());
+        //System.out.println(note.getNotetitle());
+
+        //redirectAttrs.addFlashAttribute("msg", "Successfully did it!");
+       /* RedirectView rv = new RedirectView("/home");
+        rv.setExposeModelAttributes(false);
+        rv.addStaticAttribute("msg", "Successfully did it!");*/
+        attributes.addFlashAttribute("alertClass", "success");
+        attributes.addFlashAttribute("msg", "Se agrego exitosamente");
+       // attributes.addAttribute("attribute", "redirectWithRedirectView");
+        return new RedirectView("/home");
+
+
+        //return "redirect:/";
+
+    }
+    /*
+    @PostMapping("/home")
+    public String greetingSubmit2(@ModelAttribute Files file) {
+
+
+        return "Ingreso Files";
+    }
+    */
     @RequestMapping("/home")
-    public ModelAndView getHome(Model m ){
-        Notes n = new Notes();
+    public ModelAndView getHome(@ModelAttribute("alertClass") String alertClass, @ModelAttribute("msg") String msg){
+
+        /*Notes n = new Notes();
         n.setNotetitle("Titulo 1 verdad");
         n.setNoteid(1);
         n.setNotedescription("Descrip111111111111111");
@@ -41,12 +78,17 @@ public class HomeController {
         notesMapper.insertUsers(n);
         notesMapper.insertUsers(n2);
         notesMapper.insertUsers(n3);
-        notesMapper.getNote(1);
+        notesMapper.getNote(1);*/
 
 
         ModelAndView mav = new ModelAndView();
         mav.setViewName("home");
         mav.addObject("notes", notesMapper.getNotes());
+        mav.addObject("note", new Notes());
+        mav.addObject("file", new Files());
+        mav.addObject("credential", new Credentials());
+        mav.addObject("msg", msg);
+        mav.addObject("alertClass", alertClass);
         return mav;
     }
 }
